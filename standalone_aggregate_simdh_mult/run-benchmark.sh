b6b51d5f93a73f97692577f63b5cdf487e627e47
eryilmaz <eryilmaz@node-0.rev2.openmm-pg0.wisc.cloudlab.us>
2021-01-24 15:40:59 -0600

    two new methods added linear and linear with horizontal simd

---
+++ standalone_aggregate_simdh_mult/run-benchmark.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+###########################Change this snippet
+
+#Run queries
+logGroupSizes=(2 3 4 5 6) # 7 8 9 10) # 4 8 16 32 64 128 256 512 1024)
+tuplesize=8
+size=$1
+thread=40
+
+
+mkdir $1
+for skew in 0 1.05 1.25 ; do
+	datasize=$(($size*1000*1000*1000))
+	numOfTuples=$(($datasize/$tuplesize))
+	echo "" > $1/raw_results_${size}_${skew}.txt
+	for logGroupSize in ${logGroupSizes[@]} ; do
+		groupSize=$(echo "2^${logGroupSize}" | bc)
+		make clean -C ../aggregate_simdh_mult/
+		#echo $logGroupSize
+		#echo $groupSize
+		make logGroups=${logGroupSize} -C ../aggregate_simdh_mult/	
+
+		../aggregate_simdh_mult/aggregate /slowdisk/$size/$skew/data_${groupSize}.tbl $thread $groupSize $numOfTuples &>> $1/raw_results_${size}_${skew}.txt
+
+		echo "" >> $1/raw_results_${size}_${skew}.txt
+	done
+	echo "${size}_${skew} finished"
+done
