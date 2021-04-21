e5df7a2432d39bb41aa2c8c65f8811f9fcc9aef7
Zübeyr Furkan Eryılmaz <eryilmaz@wisc.edu>
2021-04-21 12:54:27 -0500

    Delete aggregate_simd_mult directory

--- aggregate_simd_mult/pre-init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. system.inc
-
-cd bzip2-1.0.5/
-make -j install PREFIX=$PWD/../dist CFLAGS="$SYSFLAGS -O3"
-make clean
-cd ..
--- aggregate_simd_mult/simdprune.h
+++
@@ -1,6 +0,0 @@
-#include "simdprune_tables.h"
-
-static inline __m256i prune256_epi32(__m256i x, int mask) {
-  return _mm256_permutevar8x32_epi32(
-      x, _mm256_loadu_si256((const __m256i *)mask256_epi32 + mask));
-}
